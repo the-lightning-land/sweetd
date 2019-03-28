dc78b15f3096612150bc73ef7ee99bbd930e36d5
David Knezić <davidknezic@gmail.com>
2019-03-28 19:12:10 +0100

    Wrap gatt stuff into a nicer API

--- pairing/controller_linux.go
+++ pairing/controller_linux.go
@@ -2,50 +2,71 @@ package pairing
 
 import (
 	"github.com/go-errors/errors"
-	"github.com/godbus/dbus"
 	"github.com/muka/go-bluetooth/api"
-	"github.com/muka/go-bluetooth/bluez"
-	"github.com/muka/go-bluetooth/bluez/profile"
+	"github.com/muka/go-bluetooth/linux/btmgmt"
 	"github.com/muka/go-bluetooth/service"
 	"github.com/the-lightning-land/sweetd/ap"
+	"time"
 )
 
 const (
-	uuidSuffix                    = "75dd-4a0e-b688-66b7df342cc6"
-	objectName                    = "org.bluez"
-	objectPath                    = dbus.ObjectPath("/sweet/pairing/service")
-	localName                     = "Candy"
-	advertisedOptional            = true
-	uuid                          = "AAAA"
-	serviceUuid                   = "1111"
-	getNameCharacteristicUuid     = "1111"
-	getSerialNoCharacteristicUuid = "2222"
-	getIpCharacteristicUuid       = "3333"
-	getWifiSsidCharacteristicUuid = "4444"
-	scanWifiCharacteristicUuid    = "5555"
-	setWifiSsidCharacteristicUuid = "6666"
-	setWifiPskCharacteristicUuid  = "7777"
-	connectWifiCharacteristicUuid = "8888"
+	// Unique UUID suffix for the candy dispenser
+	uuidSuffix = "-75dd-4a0e-b688-66b7df342cc6"
+
+	// Where to expose the application
+	objectName = "org.bluez"
+	objectPath = "/sweet/pairing/service"
+
+	// Local name of the application
+	localName = "Candy"
+
+	// Device and service UUIDs
+	uuid        = "AAAA"
+	serviceUuid = "1111"
+
+	// Name of the device
+	// utf8s
+	deviceNameString = "2A00"
+
+	// The value of this characteristic is a UTF-8 string
+	// representing the name of the manufacturer of the device.
+	// utf8s
+	manufacturerNameString = "2A29"
+
+	// The value of this characteristic is a variable-length UTF-8 string
+	// representing the serial number for a particular instance of the device.
+	// utf8s
+	serialNumberString = "2A25"
+
+	// The value of this characteristic is a UTF-8 string
+	// representing the model number assigned by the device vendor.
+	// utf8s
+	modelNumberString = "2A24"
+
+	// The Network Availability characteristic represents
+	// if network is available or not available.
+	// uint8
+	// 0 No network available
+	// 1 One or more networks available
+	networkAvailabilityStatus = "2A3E"
+
+	ipAddress = "CA01"
+
+	wifiScanList = "CA02"
+
+	wifiSsidString = "CA03"
+
+	wifiPskString = "CA04"
+
+	wifiConnectSignal = "CA05"
 )
 
-type Characteristics struct {
-	getName     *service.GattCharacteristic1
-	getSerialNo *service.GattCharacteristic1
-	getIp       *service.GattCharacteristic1
-	getWifiSsid *service.GattCharacteristic1
-	scanWifi    *service.GattCharacteristic1
-	setWifiSsid *service.GattCharacteristic1
-	setWifiPsk  *service.GattCharacteristic1
-	connectWifi *service.GattCharacteristic1
-}
-
 type Controller struct {
-	log             Logger
-	adapterId       string
-	accessPoint     ap.Ap
-	app             *service.Application
-	service         *service.GattService1
-	characteristics *Characteristics
+	log         Logger
+	adapterId   string
+	accessPoint ap.Ap
+	app         *service.Application
+	service     *service.GattService1
 }
 
 func NewController(config *Config) (*Controller, error) {
@@ -65,102 +86,37 @@ func NewController(config *Config) (*Controller, error) {
 
 	var err error
 
-	controller.app, err = service.NewApplication(&service.ApplicationConfig{
-		UUIDSuffix: uuidSuffix,
-		UUID:       uuid,
-		ObjectName: objectName,
-		ObjectPath: objectPath,
-		LocalName:  localName,
-		ReadFunc:   controller.handleRead,
-		WriteFunc:  controller.handleWrite,
-	})
-	if err != nil {
-		return nil, errors.Errorf("Failed to initialize app: %v", err)
-	}
+	app := GattApp(uuidSuffix, uuid, objectName, objectPath, localName)
+	service := app.Service(Primary, serviceUuid, Advertised)
 
-	err = controller.app.Run()
-	if err != nil {
-		return nil, errors.Errorf("Failed to run: %v", err)
-	}
+	service.Characteristic(deviceNameString, controller.readDeviceNameString, nil)
+	service.Characteristic(manufacturerNameString, controller.readManufacturerNameString, nil)
+	service.Characteristic(serialNumberString, controller.readSerialNumberString, nil)
+	service.Characteristic(modelNumberString, controller.readModelNumberString, nil)
+	service.Characteristic(networkAvailabilityStatus, controller.readNetworkAvailabilityStatus, nil)
+	service.Characteristic(ipAddress, controller.readIpAddress, nil)
+	service.Characteristic(wifiScanList, controller.readWifiScanList, nil)
+	service.Characteristic(wifiSsidString, controller.readWifiSsidString, controller.writeWifiSsidString)
+	service.Characteristic(wifiPskString, nil, controller.writeWifiPskString)
+	service.Characteristic(wifiConnectSignal, nil, controller.writeWifiConnectSignal)
 
-	controller.service, err = controller.app.CreateService(&profile.GattService1Properties{
-		Primary: true,
-		UUID:    serviceUuid,
-	}, advertisedOptional)
+	controller.app, err = app.Run()
 	if err != nil {
-		return nil, errors.Errorf("Failed to create service: %v", err)
-	}
-
-	err = controller.app.AddService(controller.service)
-	if err != nil {
-		return nil, errors.Errorf("Failed to add service: %v", err)
-	}
-
-	controller.characteristics = &Characteristics{}
-
-	controller.characteristics.getName, err = controller.service.CreateCharacteristic(&profile.GattCharacteristic1Properties{
-		UUID: getNameCharacteristicUuid,
-		// Notifying: true,
-		Flags: []string{
-			bluez.FlagCharacteristicRead,
-			bluez.FlagCharacteristicWrite,
-		},
-	})
-	if err != nil {
-		return nil, errors.Errorf("Failed to create char: %v", err)
-	}
-
-	err = controller.service.AddCharacteristic(controller.characteristics.getName)
-	if err != nil {
-		return nil, errors.Errorf("Failed to add char: %v", err)
-	}
-
-	desc, err := controller.characteristics.getName.CreateDescriptor(&profile.GattDescriptor1Properties{
-		UUID: getNameCharacteristicUuid,
-		Flags: []string{
-			bluez.FlagDescriptorRead,
-			bluez.FlagDescriptorWrite,
-		},
-	})
-	if err != nil {
-		return nil, errors.Errorf("Failed to create char: %v", err)
-	}
-
-	err = controller.characteristics.getName.AddDescriptor(desc)
-	if err != nil {
-		return nil, errors.Errorf("Failed to add desc: %v", err)
+		return nil, errors.Errorf("Could not start app: %v", err)
 	}
 
 	return controller, nil
 }
 
-func (c *Controller) handleRead(app *service.Application, serviceUuid string, charUuid string) ([]byte, error) {
-	c.log.Infof("Reading %v", charUuid)
-	return []byte("ewfewf"), nil
-}
-
-func (c *Controller) handleWrite(app *service.Application, serviceUuid string, charUuid string, value []byte) error {
-	c.log.Infof("Writing %v = %v", charUuid, value)
-	return nil
-}
-
 func (c *Controller) Start() error {
-	// adapterID := 0
+	mgmt := btmgmt.NewBtMgmt(c.adapterId)
+	err := mgmt.Reset()
+	if err != nil {
+		return errors.Errorf("Reset %s: %v", c.adapterId, err)
+	}
 
-	//mgmt := btmgmt.NewBtMgmt(c.adapterId)
-	//err := mgmt.Reset()
-	//if err != nil {
-	//	return errors.Errorf("Reset %s: %v", c.adapterId, err)
-	//}
-	//
-	//time.Sleep(time.Millisecond * 500)
-
-	//err = linux.Up(adapterID)
-	//if err != nil {
-	//	return errors.Errorf("Failed to start device hci%d: %v", adapterID, err)
-	//}
-	//
-	//c.log.Infof("Upped")
+	// Sleep to give the device some time after the reset
+	time.Sleep(time.Millisecond * 500)
 
 	gattManager, err := api.GetGattManager(c.adapterId)
 	if err != nil {
@@ -176,35 +132,6 @@ func (c *Controller) Start() error {
 	if err != nil {
 		return errors.Errorf("Failed to advertise: %v", err)
 	}
-
-	//api.On("data", emitter.NewCallback(func(ev emitter.Event) {
-	//	print(ev.GetData())
-	//	print(ev.GetName())
-	//}))
-
-	// TODO: where to move this?
-	//err = api.On("data", emitter.NewCallback(func(ev emitter.Event) {
-	//	print(ev.GetData())
-	//	print(ev.GetName())
-	//}))
-	//if err != nil {
-	//	c.log.Errorf("Could not subscribe: %v", err)
-	//}
-	//
-	//err = c.characteristics.getName.StartNotify()
-	//if err != nil {
-	//	c.log.Errorf("Could not notify: %v", err)
-	//}
-
-	//ticker := time.NewTicker(5 * time.Second)
-	//for {
-	//	select {
-	//	case <-ticker.C:
-	//		val, err := c.characteristics.getName.ReadValue(map[string]interface{}{})
-	//		c.log.Infof("Value is: %s", val)
-	//		print(err)
-	//	}
-	//}
 
 	return nil
 }
@@ -225,5 +152,49 @@ func (c *Controller) Stop() error {
 		return errors.Errorf("Unregister failed: %v", err)
 	}
 
+	return nil
+}
+
+func (c *Controller) readDeviceNameString() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readManufacturerNameString() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readSerialNumberString() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readModelNumberString() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readNetworkAvailabilityStatus() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readIpAddress() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readWifiScanList() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) readWifiSsidString() ([]byte, error) {
+	return []byte(""), nil
+}
+
+func (c *Controller) writeWifiSsidString([]byte) error {
+	return nil
+}
+
+func (c *Controller) writeWifiPskString([]byte) error {
+	return nil
+}
+
+func (c *Controller) writeWifiConnectSignal([]byte) error {
 	return nil
 }
---
+++ pairing/gatt.go
@@ -0,0 +1,155 @@
+// Convenient methods for populating Gatt services,
+// characteristics and descriptors
+
+package pairing
+
+import (
+	"github.com/go-errors/errors"
+	"github.com/godbus/dbus"
+	"github.com/muka/go-bluetooth/bluez"
+	"github.com/muka/go-bluetooth/bluez/profile"
+	"github.com/muka/go-bluetooth/service"
+)
+
+type PrimaryType bool
+
+const Primary = PrimaryType(true)
+const Secondary = PrimaryType(false)
+
+type AdvertisedType bool
+
+const Advertised = AdvertisedType(true)
+const AdvertisedOptional = AdvertisedType(false)
+
+type HandleRead = func() ([]byte, error)
+type HandleWrite = func(value []byte) error
+
+type gattApp struct {
+	app           *service.Application
+	err           error
+	readHandlers  map[string]HandleRead
+	writeHandlers map[string]HandleWrite
+}
+
+type gattService struct {
+	*gattApp
+	service *service.GattService1
+}
+
+func GattApp(uuidSuffix string, uuid string, objectName string, objectPath string, localName string) *gattApp {
+	a := &gattApp{}
+	var err error
+
+	a.readHandlers = make(map[string]HandleRead)
+	a.writeHandlers = make(map[string]HandleWrite)
+
+	a.app, err = service.NewApplication(&service.ApplicationConfig{
+		UUIDSuffix: uuidSuffix,
+		UUID:       uuid,
+		ObjectName: objectName,
+		ObjectPath: dbus.ObjectPath(objectPath),
+		LocalName:  localName,
+		ReadFunc:   a.handleRead,
+		WriteFunc:  a.handleWrite,
+	})
+	if err != nil {
+		return &gattApp{
+			err: errors.Errorf("Could not create app: %v", err),
+		}
+	}
+
+	return a
+}
+
+func (a *gattApp) handleRead(app *service.Application, serviceUuid string, charUuid string) ([]byte, error) {
+	switch charUuid {
+	case deviceNameString:
+		return []byte(localName), nil
+	}
+
+	return []byte(""), nil
+}
+
+func (a *gattApp) handleWrite(app *service.Application, serviceUuid string, charUuid string, value []byte) error {
+	return nil
+}
+
+func (a *gattApp) Run() (*service.Application, error) {
+	if a.err != nil {
+		return nil, a.err
+	}
+
+	err := a.app.Run()
+	if err != nil {
+		return nil, errors.Errorf("Could not run app: %v", err)
+	}
+
+	return a.app, nil
+}
+
+func (a *gattApp) Service(primaryType PrimaryType, uuid string, advertised AdvertisedType) *gattService {
+	if a.err != nil {
+		return &gattService{gattApp: a}
+	}
+
+	svc, err := a.app.CreateService(&profile.GattService1Properties{
+		Primary: bool(primaryType),
+		UUID:    uuid,
+	}, bool(advertised))
+
+	if err != nil {
+		a.err = errors.Errorf("Failed to create service: %v", err)
+		return &gattService{gattApp: a}
+	}
+
+	err = a.app.AddService(svc)
+	if err != nil {
+		a.err = errors.Errorf("Failed to add service: %v", err)
+		return &gattService{gattApp: a}
+	}
+
+	return &gattService{
+		gattApp: a,
+		service: svc,
+	}
+}
+
+func (s *gattService) Characteristic(uuid string, read HandleRead, write HandleWrite) *gattService {
+	if s.err != nil {
+		return s
+	}
+
+	var inferredFlags []string
+
+	if read != nil {
+		inferredFlags = append(inferredFlags, bluez.FlagCharacteristicRead)
+
+		// TODO: Mapping by characteristic UUID only makes this work for one service
+		s.readHandlers[uuid] = read
+	}
+
+	if write != nil {
+		inferredFlags = append(inferredFlags, bluez.FlagCharacteristicWrite)
+
+		// TODO: Mapping by characteristic UUID only makes this work for one service
+		s.writeHandlers[uuid] = write
+	}
+
+	characteristic, err := s.service.CreateCharacteristic(&profile.GattCharacteristic1Properties{
+		UUID:  s.app.GenerateUUID(uuid),
+		Flags: inferredFlags,
+	})
+
+	if err != nil {
+		s.err = errors.Errorf("Failed to create characteristic: %v", err)
+		return s
+	}
+
+	err = s.service.AddCharacteristic(characteristic)
+	if err != nil {
+		s.err = errors.Errorf("Failed to add characteristic: %v", err)
+		return s
+	}
+
+	return s
+}
